7fd4103e75224e5ed23fb39a5e6ee8c227378ccb
Carine <hammescarine@gmail.com>
2017-07-22 12:29:02 -0300

    valid status 404

--- wepapp/view/home.js
+++ wepapp/view/home.js
@@ -4,45 +4,39 @@ Repositórios de um usuário: https://api.github.com/users/{username}/repos
 Detalhes de um repositório: https://api.github.com/repos/{full_name}
 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';*/
 
-let user = {};
+let userName = null;
 const searchInput = document.querySelector('.search');
 
 function displayMatches() {
 	let search = findUser + searchInput.value
 fetch( search )
 	.then(blob => blob.json())
-  	.then(function(data){
-			createProfile(data)})
-	.catch(err =>
-	console.error('Failed retrieving information', err)
-)
+  	.then(data => createProfile(data))
 }
 
 function createProfile( data ){
-	user = data
-
-	console.log(user)
-	//DocumentcreateElement
-	var html = 
+	if(data.message !== "Not Found"){
+		userName= data.login
+		var html = 
 		`
-			<img src=${user.avatar_url}></img>
+			<img src=${data.avatar_url}></img>
 			<label>name :
-				<span>${user.name}</span>
+				<span>${data.name}</span>
 			</label>
 			<label>email :
-				<span>${user.email}</span>
+				<span>${data.email}</span>
 			</label>
 			<label>folowers :
-				<span>${user.bio}</span>
+				<span>${data.bio}</span>
 			</label>
 			<label>bio :
-				<span>${user.bio}</span>
+				<span>${data.bio}</span>
 			</label>
-
 		`
-		
-	
-profile.innerHTML = html;
+		profile.innerHTML = html;
+	}
+	else
+		alert('usuário não encontrado, tente novamente')
 }
 
 
